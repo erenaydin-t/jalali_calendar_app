ec8bb594e1fb68e6f705c3e651c8dadf8fb8fb58
Eren <aydineren1986@gmail.com>
2025-08-10 00:37:27 +0300

    add version to setup

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from setuptools import setup, find_packages
+import re
+import ast
+
+# Get version from __version__ variable without importing the module
+_version_re = re.compile(r'__version__\s+=\s+(.*)')
+with open('jalali_calendar/__init__.py', 'rb') as f:
+    version = str(ast.literal_eval(_version_re.search(
+        f.read().decode('utf-8')).group(1)))
 
 with open("requirements.txt") as f:
     install_requires = f.read().strip().split("\n")
-
-# get version from __version__ variable in jalali_calendar/__init__.py
-from jalali_calendar import __version__ as version
 
 setup(
     name="jalali_calendar",
